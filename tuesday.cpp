22923f160cdfa9be8a876af1f6fd097da1777547
graj987 <graj30777@gmail.com>
2024-08-08 17:37:23 +0530

    Upload from Telegram

---
+++ tuesday.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void gautam(){
+    int a,b,c;
+    cout<<"Enter the number ,which you want table;-\n";
+    cin>>a;
+    for(int i=1; i<=10;i++){
+    	b=i*a;
+    	cout<<b<<endl;
+	}
+}
+int main(){
+	gautam();
+}
